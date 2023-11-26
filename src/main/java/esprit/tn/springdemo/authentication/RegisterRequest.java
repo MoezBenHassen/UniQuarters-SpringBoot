f4c160579d0bdf627f826df52b6a8f7a5086dc85
Nassim BenSalah <nassim.bensalah@esprit.tn>
2023-11-26 17:30:21 +0100

    finished authentication. Etudiant related customizing required.

---
+++ src/main/java/esprit/tn/springdemo/authentication/RegisterRequest.java
@@ -0,0 +1,17 @@
+package esprit.tn.springdemo.authentication;
+
+import esprit.tn.springdemo.entities.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private Role role;
+}
--- src/main/java/esprit/tn/springdemo/repositories/UserRepo.java
+++ src/main/java/esprit/tn/springdemo/repositories/UserRepo.java
@@ -3,7 +3,11 @@ package esprit.tn.springdemo.repositories;
 import esprit.tn.springdemo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
 
     Object findByUsernameAndPassword(String username, String password);
+
+    Optional<User> findByEmail(String email);
 }
