1a5c8c0fdcba8c441da4920a7fbe769752ae344f
MoezBenHassen <moezbh.mbh@gmail.com>
2023-12-03 22:34:30 +0100

    Travaux endpoint set

--- src/main/java/esprit/tn/springdemo/repositories/TravauxRepo.java
+++ src/main/java/esprit/tn/springdemo/repositories/TravauxRepo.java
@@ -1,4 +1,10 @@
 package esprit.tn.springdemo.repositories;
 
-public interface TravauxRepo {
+import esprit.tn.springdemo.entities.Travaux;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TravauxRepo extends CrudRepository<Travaux, Integer> {
+    List<Travaux> findAllByBloc(int tribunal);
 }
